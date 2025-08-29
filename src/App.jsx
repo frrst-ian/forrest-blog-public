d70c89dcbb7765f7753c2e9069e411a9487abbeb
frrst-ian <frrstxd@proton.me>
2025-08-29 15:08:28 +0800

    feat: add crucial blog components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const PostItem = ({ post }) => {
   return (
-    <>
-      <p>Hello Front End</p>
-    </>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+      <p>{post.createdAt}</p>
+      <hr />
+    </div>
   );
-}
+};
 
-export default App;
+const PostList = ({ posts }) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+const POSTS = [
+  {
+    id: 1,
+    title: "Sample Title",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdAt: Date.now(),
+  },
+  {
+    id: 2,
+    title: "Sample Title 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdAt: Date.now(),
+  },
+
+  {
+    id: 3,
+    title: "Sample Title 2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdAt: Date.now(),
+  },
+  {
+    id: 4,
+    title: "Sample Title 3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdAt: Date.now(),
+  },
+];
+
+const App = () => {
+  return <PostList posts={POSTS} />;
+};
+
+export { App };
