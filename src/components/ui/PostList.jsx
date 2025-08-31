d1b085f991a9fed26c2a85ddab5693bfcc0f1e0c
frrst-ian <frrstxd@proton.me>
2025-08-31 15:55:10 +0800

    chore: rename postList title and page title

--- src/components/ui/PostList.jsx
+++ src/components/ui/PostList.jsx
@@ -4,7 +4,7 @@ import "../../styles/components/PostList.css"
 const PostList = ({ posts }) => {
   return (
     <div className="postList">
-      <h1 className="postList_title">Forrest's Blog Posts</h1>
+      <h1 className="postList_title">Ian Forrest's Blog Posts</h1>
       {posts.map((post) => (
         <PostItemContainer key={post.id} post={post} />
       ))}
